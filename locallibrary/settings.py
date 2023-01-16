4eb72aa797e7f24191c6066b8361d90f381a48f2
mo <muhammedqaid23@gmail.com>
2023-01-16 21:29:01 +0000

    Trying to fix incorrect csrf url from last push

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -41,7 +41,7 @@ ALLOWED_HOSTS = ['djangolocallibrary-production.up.railway.app', '127.0.0.1']
 
 ## For example, for a site URL is at 'web-production-3640.up.railway.app'
 ## (replace the string below with your own site URL):
-CSRF_TRUSTED_ORIGINS = ['djangolocallibrary-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://djangolocallibrary-production.up.railway.app']
 
 # During development/for this tutorial you can instead set just the base URL
 # CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
